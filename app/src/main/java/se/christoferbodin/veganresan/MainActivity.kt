e90e4fd63b68e3ff87b8152bf520c7bb8d2a5d6a
Christofer Bodin <christofer@christoferbodin.se>
2019-01-05 02:07:07 +0100

    Add a blank AppBar.

--- app/src/main/java/se/christoferbodin/veganresan/MainActivity.kt
+++ app/src/main/java/se/christoferbodin/veganresan/MainActivity.kt
@@ -1,5 +1,12 @@
 package se.christoferbodin.veganresan
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity()
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.appbar))
+    }
+}
