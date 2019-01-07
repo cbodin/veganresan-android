f0aff335267b3cc8eb43c78908d5e515c795ade5
Christofer Bodin <christofer@christoferbodin.se>
2019-01-07 22:41:51 +0100

    Move API creation to a custom Application class. Simplify MealViewModel with added error and loading LiveData.

---
+++ app/src/main/java/se/christoferbodin/veganresan/VeganApplication.kt
@@ -0,0 +1,27 @@
+package se.christoferbodin.veganresan
+
+import android.app.Application
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import se.christoferbodin.veganresan.api.MealApi
+import java.util.Date
+
+class VeganApplication: Application() {
+    val mealApi: MealApi by lazy<MealApi> {
+        val moshi = Moshi.Builder()
+            .add(Date::class.java, Rfc3339DateJsonAdapter())
+            .build()
+
+        val baseUrl = getString(R.string.api_base_url)
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+
+        retrofit.create(MealApi::class.java)
+    }
+}
--- app/src/main/java/se/christoferbodin/veganresan/viewmodel/MealViewModel.kt
+++ app/src/main/java/se/christoferbodin/veganresan/viewmodel/MealViewModel.kt
@@ -1,50 +1,52 @@
 package se.christoferbodin.veganresan.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import se.christoferbodin.veganresan.VeganApplication
 import se.christoferbodin.veganresan.api.MealApi
 import se.christoferbodin.veganresan.model.Meal
-import java.util.Date
 
-class MealViewModel : ViewModel() {
-    private val api: MealApi
-    private var mealsJob: Deferred<List<Meal>>? = null
-    val meals = MutableLiveData<List<Meal>>()
+class MealViewModel(application: Application) : AndroidViewModel(application) {
+    private val api: MealApi = getApplication<VeganApplication>().mealApi
+    private val meals = MutableLiveData<List<Meal>>()
+    private val mealsError = MutableLiveData<Boolean>().apply { value = false }
+    private val mealsLoading = MutableLiveData<Boolean>().apply { value = false }
 
-    init {
-        // TODO: Inject using Dagger or some other method?
-        val moshi = Moshi.Builder()
-            .add(Date::class.java, Rfc3339DateJsonAdapter())
-            .build()
+    fun meals(refresh: Boolean = false): LiveData<List<Meal>> {
+        if (mealsLoading.value == true) {
+            return meals
+        }
 
-        val retrofit = Retrofit.Builder()
-            // TODO: Move to values xml
-            .baseUrl("https://veganresan.christoferbodin.se")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .addCallAdapterFactory(CoroutineCallAdapterFactory())
-            .build()
+        // Only fetch if forcing a refresh or if we have no data
+        if (refresh || meals.value == null) {
+            mealsLoading.value = true
+            mealsError.value = false
 
-        api = retrofit.create(MealApi::class.java)
-    }
-
-    fun loadMeals(refresh: Boolean = false) {
-        val isLoading = mealsJob?.isActive ?: false
-        val shouldRefresh = refresh || meals.value == null
-
-        if (!isLoading && shouldRefresh) {
             CoroutineScope(Dispatchers.Main).launch {
-                mealsJob = api.mealList()
-                meals.value = mealsJob?.await()
+                try {
+                    meals.value = api.mealList().await()
+                } catch (error: Exception) {
+                    mealsError.value = true
+                }
+
+                mealsLoading.value = false
             }
         }
+
+        // Return immutable LiveData
+        return meals
+    }
+
+    fun mealsError(): LiveData<Boolean> {
+        return mealsError
+    }
+
+    fun mealsLoading(): LiveData<Boolean> {
+        return mealsLoading
     }
 }
