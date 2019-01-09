efdeb0a0e39dabbd0699ac1c0948a768e54dc896
Christofer Bodin <christofer@christoferbodin.se>
2019-01-10 00:37:31 +0100

    Add AppBar menu with upload icon.

--- app/src/main/java/se/christoferbodin/veganresan/MainActivity.kt
+++ app/src/main/java/se/christoferbodin/veganresan/MainActivity.kt
@@ -1,6 +1,7 @@
 package se.christoferbodin.veganresan
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
@@ -8,5 +9,10 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.appbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true
     }
 }
