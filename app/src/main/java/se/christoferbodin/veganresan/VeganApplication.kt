21494d2a4a226362c8263e8e3d5ac781d895861b
Christofer Bodin <christofer@christoferbodin.se>
2019-01-17 22:45:39 +0100

    Add authorization header to API calls when signed in.

--- app/src/main/java/se/christoferbodin/veganresan/VeganApplication.kt
+++ app/src/main/java/se/christoferbodin/veganresan/VeganApplication.kt
@@ -1,22 +1,39 @@
 package se.christoferbodin.veganresan
 
 import android.app.Application
+import android.preference.PreferenceManager
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import se.christoferbodin.veganresan.api.MealApi
 import java.util.Date
 
-class VeganApplication: Application() {
+class VeganApplication : Application() {
     val mealApi: MealApi by lazy {
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                var request = chain.request()
+
+                getUploadPassword()?.apply {
+                    request = request.newBuilder()
+                        .addHeader("Authorization", "Bearer $this")
+                        .build()
+                }
+
+                chain.proceed(request)
+            }
+            .build()
+
         val moshi = Moshi.Builder()
             .add(Date::class.java, Rfc3339DateJsonAdapter())
             .build()
 
         val baseUrl = getString(R.string.api_base_url)
         val retrofit = Retrofit.Builder()
+            .client(client)
             .baseUrl(baseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
@@ -24,4 +41,7 @@ class VeganApplication: Application() {
 
         retrofit.create(MealApi::class.java)
     }
+
+    private fun getUploadPassword(): String? = PreferenceManager.getDefaultSharedPreferences(this)
+        .getString("UPLOAD_PASSWORD", null)
 }
